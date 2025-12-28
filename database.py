6ae5a522fa45e628ca7ef6519714c5da955aeb46
Aleksey Kurenkov <alexleoman@mail.ru>
2025-12-28 11:48:59 +0500

    Create crud for notification servise api - refactoring2

--- database.py
+++ database.py
@@ -1,5 +1,3 @@
-"""from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase"""
 from typing import List
 from schema import NotificationResponse
 
