96fd17401f4455df63786a01b817588293a55439
Aleksey Kurenkov <alexleoman@mail.ru>
2026-01-04 12:51:41 +0500

    notification servise api-asyncio refactoring2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from datetime import datetime
+from typing import List
+from uuid import uuid4, UUID
+
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException, status, Depends, Response
 from sqlalchemy import select
-# from sqlalchemy.ext.asyncio import AsyncSession
-from typing import List
-from uuid import uuid4, UUID
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import engine, Base, get_db
 from models import Notification
@@ -13,12 +14,9 @@ from schema import NotificationCreate, NotificationResponse, NotificationUpdate
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Асинхронное создание таблиц
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
-
-# Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Notification Service API.", lifespan=lifespan)
 
@@ -28,7 +26,8 @@ app = FastAPI(title="Notification Service API.", lifespan=lifespan)
         response_model=NotificationResponse,
         status_code=status.HTTP_201_CREATED
     )
-async def create_notification(note: NotificationCreate, db=Depends(get_db)):
+async def create_notification(note: NotificationCreate,
+                              db: AsyncSession = Depends(get_db)):
     db_note = Notification(
         id=uuid4(),
         user_id=note.user_id,
@@ -42,36 +41,38 @@ async def create_notification(note: NotificationCreate, db=Depends(get_db)):
 
 
 @app.get("/notifications", response_model=List[NotificationResponse])
-async def get_all_notifications(db=Depends(get_db)):
+async def get_all_notifications(db: AsyncSession = Depends(get_db)):
     query = select(Notification)
     note = await db.scalars(query)
     notifications = note.all()
     return notifications
 
 
-@app.get("/notifications/{note_id}", response_model=NotificationResponse)
-async def get_notification(note_id: UUID, db=Depends(get_db)):
-    get_note = select(Notification).where(Notification.id == note_id)
-    # get_note = db.get(Notification, note_id)
-    result = await db.execute(get_note)
-    note = result.scalar_one_or_none()
+async def get_notification_or_404(
+    note_id: UUID,
+    db: AsyncSession = Depends(get_db)
+) -> Notification:
+    note = await db.get(Notification, note_id)
     if note is None:
-        raise HTTPException(status_code=404, detail="Notification not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Notification not found")
     return note
+
+
+@app.get("/notifications/{note_id}", response_model=NotificationResponse)
+async def get_notification(note_id: UUID,
+                           note:
+                           Notification = Depends(get_notification_or_404)):
+    return Response(status_code=200)
 
 
 @app.put("/notifications/{note_id}", response_model=NotificationResponse)
 async def update_notification_full(
     note_id: UUID,
     item: NotificationCreate,
-    db=Depends(get_db)
+    note: Notification = Depends(get_notification_or_404),
+    db: AsyncSession = Depends(get_db)
 ):
-    get_note = select(Notification).where(Notification.id == note_id)    
-    result = await db.execute(get_note)
-    note = result.scalar_one_or_none()
-    if note is None:
-        raise HTTPException(status_code=404, detail="Notification not found")
-
     note.user_id = item.user_id
     note.notification = item.notification
     note.send_at = item.send_at
@@ -84,16 +85,10 @@ async def update_notification_full(
 async def update_notification_partial(
     note_id: UUID,
     item: NotificationUpdate,
-    db=Depends(get_db)
+    note: Notification = Depends(get_notification_or_404),
+    db: AsyncSession = Depends(get_db)
 ):
-    get_note = select(Notification).where(Notification.id == note_id)
-
     update_data = item.model_dump(exclude_unset=True)
-    result = await db.execute(get_note)
-    note = result.scalar_one_or_none()
-    if note is None:
-        raise HTTPException(status_code=404, detail="Notification not found")
-
     for key, value in update_data.items():
         if isinstance(value, datetime) and value.tzinfo is not None:
             value = value.replace(tzinfo=None)
@@ -105,13 +100,10 @@ async def update_notification_partial(
 
 
 @app.delete("/notifications/{note_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_notification(note_id: UUID, db=Depends(get_db)):
-    get_note = select(Notification).where(Notification.id == note_id)
-    # get_note = db.get(Notification, note_id)
-    result = await db.execute(get_note)
-    note = result.scalar_one_or_none()
-    if note is None:
-        raise HTTPException(status_code=404, detail="Notification not found")
+async def delete_notification(note_id: UUID,
+                              note:
+                              Notification = Depends(get_notification_or_404),
+                              db: AsyncSession = Depends(get_db)):
     await db.delete(note)
     await db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
